80df806e615e259ebee82bc7eeec76862d91741e
OziasMumbongo <oziasmumbongo67@gmail.com>
2025-09-21 22:33:23 +0200

    updated 9

--- server.js
+++ server.js
@@ -523,6 +523,6 @@ app.delete('/Orders/:id', async (req, res) => {
 
 app.listen(port, '0.0.0.0', async () => {
   await connectToMongodb();
-  console.log(`Server running at http://0.0.0.0:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
 
